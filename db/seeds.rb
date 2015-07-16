1da4502e0bf7c733c0472483f25cc357f12ebaea
Josh Arnold <joshuaarnold22@gmail.com>
2015-07-15 22:45:25 -0400

    Seeding Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
 
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: 'helloworld'
+  )
+  user.save!
+end
+ 
 users = User.all
 
+5.times do
+  Item.create(
+    user: users.sample,
+    name: Faker::Lorem.sentence
+ )
+end
+
+items = Item.all
+
+#junior_miller@price.name
